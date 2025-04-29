3ff90374d2909e3fc68c9cf6c7c058438d366897
Ryusen9 <asratul02@gmail.com>
2025-04-29 22:31:39 +0600

    Firebase integration

--- src/Context/ContextProvide.jsx
+++ src/Context/ContextProvide.jsx
@@ -1,15 +1,39 @@
 import React, { useEffect, useState } from "react";
 import Context from "./Context";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import auth from "../Firebase/Firebase.config";
 
 const ContextProvide = ({ children }) => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
   useEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
     localStorage.setItem("theme", theme);
   }, [theme]);
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+    if(currentUser) {
+      setLoading(false);
+    } else {
+      setLoading(false);
+    }
+    })
+    return () => {
+      unsubscribe()
+    }
+  }, [])
   const value = {
     theme,
     setTheme,
+    createUser,
+    loading,
+    user,
   };
   return <Context.Provider value={value}>{children}</Context.Provider>;
 };
